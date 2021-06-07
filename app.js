3540140724e82f47f8e51e7a73f0f38cf1f0bcbd
Dennis <sigur@mailbox.org>
2021-06-07 18:12:52 +0200

    updated app.js - specified heroku server

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/index.html");
     app.use(express.static("public"));
 })
 
 
-app.listen(3000, function () {
-    console.log("server is listening to port 3000");
-})
+app.listen(port);
